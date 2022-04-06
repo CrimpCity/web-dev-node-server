04fee022258b54934dbed77245d2f7ab1002a431
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-06 15:58:17 -0400

    add part 3.0 hello controller

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-// const express = require('express');
 import express from 'express';
+// const express = require('express');
+import helloController from "./controllers/hello-controller.js"
+
 const app = express();
-app.get('/hello', (req, res) => { res.send('Hello World!') })
-app.get('/', (req, res) => { res.send('Welcome to Full Stack Development!') })
+
+helloController(app)
+// app.get('/hello', (req, res) => { res.send('Hello World!') })
+// app.get('/', (req, res) => { res.send('Welcome to Full Stack Development!') })
 app.listen(4000);
