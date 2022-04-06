62d54e2fcfaec40ff40acc0d9f0b03becc0c89a4
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-06 16:51:54 -0400

    add part 3.2 query param

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,10 +9,28 @@ const userController = (app) => {
 }
 
 // function runs when /api/users requested
+// const findAllUsers = (req, res) => {
+//     // responds with array of users
+//     res.json(users);
+// }
+
+
+const findUsersByType = (type) => {
+    return users.filter(user => user.type === type);
+}
+
+
+
+
 const findAllUsers = (req, res) => {
-    // responds with array of users
+    const type = req.query.type;              // retrieve type parameter from query
+    if (type) {                               // if type parameter in query
+        res.json(findUsersByType(type));      // find all users of that type and respond       
+        return;                               // return so it doesn't continue   
+    }
     res.json(users);
 }
+
 
 // exports so server.js can import
 export default userController;
