1f490525384e43ceece56a67b63bd93c0635a46d
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-06 15:33:55 -0400

    add part 2.7 config node ES6

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
 const app = express();
 app.get('/hello', (req, res) => { res.send('Hello World!') })
 app.get('/', (req, res) => { res.send('Welcome to Full Stack Development!') })
