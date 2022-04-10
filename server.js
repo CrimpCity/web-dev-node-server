27e167494cc5e8b4d090c992ab8e9e8efc7b602d
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-10 18:09:34 -0400

    add mongo cloud connection

--- server.js
+++ server.js
@@ -8,10 +8,24 @@ import userController from './controllers/user-controller.js';
 import tuitsController from './controllers/tuits-controller.js';
 import mongoose from 'mongoose';
 
+
+// SET UP DATABASE CONNECTION
 // LOCAL DEVELOPMENT
-mongoose.connect('mongodb://localhost:27017/webdev');
+// mongoose.connect('mongodb://localhost:27017/webdev');
+
+// Mongo Cloud Deployment
+const PROTOCOL = "mongodb+srv";
+const DB_USERNAME = "Georgian"
+const DB_PASSWORD = "987654321"
+const HOST = "cluster0.itxlc.mongodb.net";
+const DB_NAME = "myFirstDatabase";
+const DB_QUERY = "retryWrites=true&w=majority";
+const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
+mongoose.connect(connectionString);
 
 
+
+// SET UP APP
 const app = express();
 
 app.use(cors());
@@ -21,7 +35,4 @@ helloController(app)
 userController(app)
 tuitsController(app)
 
-// app.get('/hello', (req, res) => { res.send('Hello World!') })
-// app.get('/', (req, res) => { res.send('Welcome to Full Stack Development!') })
-// app.listen(4000);
 app.listen(process.env.PORT || 4000);
