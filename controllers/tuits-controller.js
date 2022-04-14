666f64e90db4a04e00c0082b1444c0e711a16526
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-14 18:50:09 -0400

    fix updateTuit return

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -1,8 +1,4 @@
-// import posts from "./tuits/tuits.js";
-// let tuits = posts;
-
-import tuitsDao from "../daos/tuitsDao.js";
-
+import * as tuitsDao from "../daos/tuitsDao.js";
 
 
 const tuitsController = (app) => {
@@ -12,38 +8,22 @@ const tuitsController = (app) => {
     app.delete('/api/tuits/:tid', deleteTuit);
 }
 
-// const findAllTuits = (req, res) => { res.json(tuits); }
+
 const findAllTuits = async (req, res) => {
     const tuits = await tuitsDao.findAllTuits()
     res.json(tuits);
 }
 
 
-// THIS USES posts from "./tuits/tuits.js"
-// const createTuit = (req, res) => {
-//     const newTuit = req.body;
-//     newTuit._id = (new Date()).getTime() + '';
-//     newTuit.stats = { comments: 0, retuits: 0, likes: 0, dislikes: 0 };
-//     tuits.push(newTuit);
-//     res.json(newTuit);
-// }
-
 const createTuit = async (req, res) => {
     const newTuit = req.body;
-    // make usernames unique
+    // make usernames unique since no login session info yet
     newTuit.postedBy = { username: (new Date()).getTime().toString() };
     newTuit.stats = { comments: 0, retuits: 0, likes: 0, dislikes: 0 };
     const insertedTuit = await tuitsDao.createTuit(newTuit);
     res.json(insertedTuit);
 }
 
-
-// THIS USES posts from "./tuits/tuits.js"
-// const deleteTuit = (req, res) => {
-//     const tuitdIdToDelete = req.params.tid;
-//     tuits = tuits.filter(t => t._id !== tuitdIdToDelete);
-//     res.sendStatus(200);
-// }
 
 const deleteTuit = async (req, res) => {
     const tuitdIdToDelete = req.params.tid;
@@ -52,29 +32,12 @@ const deleteTuit = async (req, res) => {
 }
 
 
-// // THIS USES posts from "./tuits/tuits.js"
-// // Updates ONLY tuitText and stats attributes
-// const updateTuit = (req, res) => {
-//     const tuitdIdToUpdate = req.params.tid;
-//     const updatedTuit = req.body;
-//     tuits.forEach(t => t._id === tuitdIdToUpdate ? (
-//         t.tuitText = updatedTuit.tuitText, t.stats = updatedTuit.stats) : t)
-//     res.sendStatus(200);
-// }
-
 const updateTuit = async (req, res) => {
     const tuitdIdToUpdate = req.params.tid;
     const updatedTuit = req.body;
     const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updatedTuit);
-    res.sendStatus(status);
+    res.send(status);
 }
-
-// const updateStats = async (req, res) => {
-//     const tuitdIdToUpdate = req.params.tid;
-//     const newStats = req.body;
-//     const status = await tuitsDao.updateTuitStats(tuitdIdToUpdate, newStats);
-//     res.sendStatus(status);
-// }
 
 // exports so server.js can import
 export default tuitsController;
